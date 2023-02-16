2ce0e2eaa0cc66bd0a7ac527e4fc23abe3ab4b41
svyat174 <110224503+svyat174@users.noreply.github.com>
2023-02-16 17:09:35 +0300

    new brunch reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Header } from './components/Header';
 import { Main } from './components/Main';
 import { Footer } from './components/Footer';
+import { ContextProvider } from './context';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <ContextProvider>
+        <Main />
+      </ContextProvider>
       <Footer />
     </div>
   );
